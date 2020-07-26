ff2967e11464f5873bb2f1a08b9c95859bdfac27
joh-robbins <joh.robbins.au@gmail.com>
2020-07-27 00:03:48 +1000

    Add Favorite model

---
+++ zappy/Models/Favorite.swift
@@ -0,0 +1,9 @@
+//
+//  Created by Joh Robbins on 26/7/20.
+//  Copyright © 2020 Joh Robbins. All rights reserved.
+//
+
+struct Favorite: Codable {
+  var location: Location
+  var forecast: Forecast
+}
---
+++ zappy/Models/Forecast.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Joh Robbins on 26/7/20.
+//  Copyright © 2020 Joh Robbins. All rights reserved.
+//
+
+enum Forecast: String, Codable {
+    case current = "Current weather"
+    case twentyFourHour = "24-hour forecast"
+    case sevenDay = "7-day forecast"
+}
