ad42508cbab13a35ebea2904cb450cbd9de810de
joh-robbins <joh.robbins.au@gmail.com>
2020-09-06 02:41:53 +1000

    Display favorite forecast and location on load

--- zappy/Controllers/CurrentWeatherViewController.swift
+++ zappy/Controllers/CurrentWeatherViewController.swift
@@ -6,62 +6,65 @@
 import UIKit
 
 class CurrentWeatherViewController: UIViewController {
-  @IBOutlet private var favouriteBarButtonItem: UIBarButtonItem!
-  @IBOutlet private var cityLabel: UILabel!
-  @IBOutlet private var contentStackView: UIStackView!
+    @IBOutlet private var favouriteBarButtonItem: UIBarButtonItem!
+    @IBOutlet private var cityLabel: UILabel!
+    @IBOutlet private var contentStackView: UIStackView!
 
-  var forecastPeriod: ForecastPeriod?
-  var location: Location!
-  var isFavourited = false;
+    var forecastPeriod: ForecastPeriod?
+    var location: Location!
+    var isFavourited = false;
 
-  private let userPreferences = UserPreferences()
-  private let weatherService = APIService()
+    private let userPreferences = UserPreferences()
+    private let weatherService = APIService()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupUI()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
 
-    weatherService.getWeather(for: location) { result in
-      DispatchQueue.main.async {
-        switch result {
-        case .success(let weather):
-          let currentWeatherView = CurrentWeatherView(frame: CGRect(x: 0, y: 0, width: self.contentStackView.frame.width, height: 200))
-          self.contentStackView.addSubview(currentWeatherView)
-          currentWeatherView.configure(weather.currently)
+        weatherService.getWeather(for: location) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let weather):
+                    let currentWeatherView = CurrentWeatherView(frame: CGRect(x: 0, y: 0, width: self.contentStackView.frame.width, height: 200))
+                    self.contentStackView.addSubview(currentWeatherView)
+                    currentWeatherView.configure(weather.currently)
 
-        case .failure(let error):
-            print(error)
+                case .failure(let error):
+                    print(error)
+                }
+            }
         }
-      }
     }
-  }
 
-  @IBAction func toggleFavourite(_ sender: UIBarButtonItem) {
-    isFavourited.toggle()
-    animateFavouriteIcon(isFavourited)
-    if isFavourited {
-      guard let location = location, let forecastPeriod = forecastPeriod else { return }
-      let favourite = Favourite(location: location, forecastPeriod: forecastPeriod)
-      userPreferences.saveFavourite(with: favourite)
-    } else {
-      userPreferences.removeFavourite()
+    @IBAction func backButtonTapped(_ sender: Any) {
+      returnToForecastSelector(for: location)
     }
-  }
 
-  private func setupUI() {
-    navigationItem.title = forecastPeriod?.rawValue
-    animateFavouriteIcon(isFavourited)
-
-    guard let location = location else { return }
-    cityLabel.text = location.city
-  }
-
-  private func animateFavouriteIcon(_ fill: Bool) {
-    if fill {
-      favouriteBarButtonItem.image = UIImage(systemName: "star.fill")
-    } else {
-      favouriteBarButtonItem.image = UIImage(systemName: "star")
+    @IBAction func toggleFavourite(_ sender: UIBarButtonItem) {
+        isFavourited.toggle()
+        animateFavouriteIcon(isFavourited)
+        if isFavourited {
+            guard let location = location, let forecastPeriod = forecastPeriod else { return }
+            let favourite = Favourite(location: location, forecastPeriod: forecastPeriod)
+            userPreferences.saveFavourite(with: favourite)
+        } else {
+            userPreferences.removeFavourite()
+        }
     }
-  }
+
+    private func setupUI() {
+        animateFavouriteIcon(isFavourited)
+
+        guard let location = location else { return }
+        cityLabel.text = location.city
+    }
+
+    private func animateFavouriteIcon(_ fill: Bool) {
+        if fill {
+            favouriteBarButtonItem.image = UIImage(systemName: "star.fill")
+        } else {
+            favouriteBarButtonItem.image = UIImage(systemName: "star")
+        }
+    }
 }
 
---
+++ zappy/Extensions/UIViewController+ReturnToForecastSelector.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Joh Robbins on 6/9/20.
+//  Copyright © 2020 Joh Robbins. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func returnToForecastSelector(for location: Location) {
+        if let navigationController = self.navigationController {
+          navigationController.popViewController(animated: true)
+
+          let forecastSelectorViewController =  navigationController.topViewController as! ForecastSelectorViewController
+          forecastSelectorViewController.location = location
+        }
+    }
+}
